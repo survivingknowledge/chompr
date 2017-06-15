53cc4f4508df28c5ebba944c0894e4cced46f3dc
Jordan Sleeper <survivingknowledge@gmail.com>
2017-06-15 17:02:33 -0400

    food controller basic functionality no permissions or auth yet

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,54 @@
 class FoodsController < ApplicationController
+
+  before_action :set_food, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to @food, notice: 'Food successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to @food, notice: 'Food successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @food
+      @food.destroy
+    end
+    redirect_to foods_path
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(
+      :name
+    )
+  end
 end
