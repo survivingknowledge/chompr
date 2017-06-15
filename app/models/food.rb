a4ee23b212bd028f801276cc2cdcd571f007deb7
Jordan Sleeper <survivingknowledge@gmail.com>
2017-06-15 19:44:01 -0400

    stat to ratio dynamically calls model attribtue

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,12 +3,18 @@ class Food < ApplicationRecord
   has_many :meals, through: :foods_meals
 
   def serving_ratio
-    ratio = 0
+    ratio = 0.0
     #have to transform to float to maintain decimal precision
     if self.serving_size && self.calories && self.serving_size > 0 && self.calories > 0
         ratio = self.calories.to_f/self.serving_size.to_f
     end
     ratio
+  end
+
+  def stat_to_ratio(stat)
+    if self.respond_to?(stat)
+      send(stat).to_f * serving_ratio
+    end
   end
 
 end
