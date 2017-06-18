d4be9f719bf8396c638ee4c9ae483005e327ffe9
Jordan Sleeper <survivingknowledge@gmail.com>
2017-06-18 19:35:20 -0400

    Food for meals added through session
    disabled turbolinks as it was causing invalid token authenticity when it reloaded page through xhr for form, still a bug in 5.1
    
    need to refactor ad code is duplicated in controller and meals#totals but totals cant work on array

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,13 +1,28 @@
 class MealsController < ApplicationController
 
     before_action :set_meal, only: [:show, :edit, :update, :destroy]
+    before_action :set_session_current_meal
 
     def index
       @meals = Meal.all
     end
 
     def new
-      @meal = Meal.new
+      @totals = {}
+      ignored = %w[id name serving_type serving_size created_at updated_at]
+
+      @current_meal = session[:current_meal].collect do |food_id|
+        food = Food.find_by_id(food_id)
+
+        food.attributes.each do |key, value|
+          if !ignored.include?(key)
+            @totals[key] ? @totals[key] += (value.to_f) : @totals[key] = value
+          end
+        end
+        #make sure to return food
+        food
+      end
+
     end
 
     def create
@@ -40,6 +55,12 @@ class MealsController < ApplicationController
       redirect_to meals_path
     end
 
+    def add_food
+      @food = Food.find_by_id(params[:food_id])
+      session[:current_meal] << @food.id if @food
+      redirect_to new_meal_path
+    end
+
     private
 
     def set_meal
@@ -51,6 +72,10 @@ class MealsController < ApplicationController
                               :name,
                               food_ids: []
       )
+    end
+
+    def set_session_current_meal
+      session[:current_meal] ||= []
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :meals
   resources :foods
+  post 'meals/add/:food_id', to: 'meals#add_food', as: 'add_food'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
